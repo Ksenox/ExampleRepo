77e0379aaaeb14e14caa69ecce04063957beb840
Ksenox <xxxtestxxx@yandex.ru>
2020-09-28 17:34:12 +0300

    First program

---
+++ lb1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[100];
+    char sym = ' ';
+    int arr_size = 0;
+
+    printf("Hello, User!\n");
+
+    while(arr_size < 100 && sym == ' '){
+        scanf("%d%c",&arr[arr_size++],&sym);
+    }
+
+    for(int i = arr_size-1; i >= 0; i--){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
