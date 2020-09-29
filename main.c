e66d4a9f60f78a83d5e8c3927988d6ae5dd260bf
Ksenox <xxxtestxxx@yandex.ru>
2020-09-29 17:36:50 +0300

    Changes

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+int f1(int value){
+    return -value;
+}
+
 int main(){
 
     int arr[100];
@@ -11,8 +15,12 @@ int main(){
         scanf("%d%c",&arr[arr_size++], &sym);
     }
 
-    for(int i = arr_size - 1; i >= 0; i--){
-        printf("{%d}",arr[i]);
+    for(int i = 0; i < arr_size; i++){
+        arr[i] = f1(arr[i]);
+    }
+
+    for(int i = 0; i < arr_size; i++){
+        printf("%d ",arr[i]);
     }
 
     return 0;
